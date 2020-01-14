fb5266182eec45e2a9f8939f044dd88b7ea00dcd
Laura Hartstein <lauri@Lauras-MBP.fritz.box>
2020-01-14 10:03:45 +0100

    Aufgabe 5 mit Farbveraenderung

--- Aufgabe5/aufgabe5.php
+++ Aufgabe5/aufgabe5.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <title>Aufgabe5</title>
+    <style>
+        tbody{
+            color: rebeccapurple;
+        }
+        td:hover{
+            background-color: #a94442;
+        }
+
+    </style>
     <?php
     function zufzahl($max, $anzahl, $stellen){
-        echo "<table>";
+        echo "<table class='table table-striped table-hover'>";
         echo "<thead> <tr>
         <th>Zufallszahl</th>";
     for($j=1; $j<=$stellen; $j++){
@@ -15,19 +25,25 @@
     echo "</tr> </thead> <tbody>";
         for($i=1; $i<$anzahl; $i++) {
             $zzahl = rand(1, $max);
-            if ($zzahl > 1000)
                 echo "<tr>";
-            elseif ($zzahl > 10000) echo "<tr style='background-color: #6f42c1;'>";
-            elseif ($zzahl > 100000) echo "<tr style='background-color: #0c5460'>";
 
             /*$gerundet2 = abschneiden($zzahl, 2);
             $gerundet3 = abschneiden($zzahl, 3);
             echo $zzahl, $gerundet2, $gerundet3."<br>";
             */
-            echo "<td>$zzahl</td>";
+            if ($zzahl > 1000)
+            echo "<td style='background-color: #9fcdff'>$zzahl</td>";
+            elseif ($zzahl > 500)
+                echo "<td style='background-color: burlywood'>$zzahl</td>";
 
+            else echo "<td style='background-color: darkseagreen'>$zzahl</td>";
             for ($j = 1; $j <= $stellen; $j++) {
-                echo "<td>" . abschneiden($zzahl, $j) . "</td>";
+                if($zzahl >1000)
+                echo "<td style='background-color: #9fcdff'>" . abschneiden($zzahl, $j) . "</td>";
+                elseif ($zzahl >500)
+                    echo "<td style='background-color: burlywood'>" . abschneiden($zzahl, $j) . "</td>";
+                else
+                    echo "<td style='background-color: darkseagreen'>" . abschneiden($zzahl, $j) . "</td>";
             }
             echo "</tr>";
         }
@@ -38,7 +54,10 @@
         $base = pow(10, $stellen);
         return $zahl - ($zahl % $base);
     }
+
+
     ?>
+
 </head>
 <body>
 <h1>Zufallszahlen</h1>
@@ -47,4 +66,5 @@
     zufzahl(2000, 20, 3);?>
 </div>
 </body>
+
 </html>
